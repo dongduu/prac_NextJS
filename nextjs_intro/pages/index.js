a1c15dc85c225f14f6e7cedd2f6e2fbc72baf30e
dongduu <4012popo@naver.com>
2022-06-21 14:59:03 +0900

    feat: fetching data

--- nextjs_intro/pages/index.js
+++ nextjs_intro/pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import Title from "../components/Title";
 
+const API_KEY = "cd124d01880b35b0167250cac4dba58a";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `http://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Title title="Home" />
-      <h1>Hello!</h1>
+      {movies?.map((item) => (
+        <div key={item.id}>
+          <h4>{item.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
