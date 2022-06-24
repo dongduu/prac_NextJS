455a942c77013d05685183262b4f76e8ff36a032
dongduu <4012popo@naver.com>
2022-06-24 15:59:55 +0900

    feat: catch all

--- nextjs_intro/pages/index.js
+++ nextjs_intro/pages/index.js
@@ -5,15 +5,7 @@ import Title from "../components/Title";
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
   return (
     <div className="container">
@@ -27,13 +19,7 @@ export default function Home({ results }) {
           <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} />
           <h4>
             <Link
-              href={{
-                pathname: `/movies/${item.id}`,
-                query: {
-                  title: item.original_title,
-                },
-              }}
-              as={`/movies/${item.id}`}
+              href={`/movies/${item.original_title}/${item.id}`}
               key={item.id}
             >
               <a>{item.original_title} </a>
